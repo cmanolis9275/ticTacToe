6cee7b99f5127e725a9fb605017d1270395b831c
cmanolis9275 <cmanolis9275@bg-mmcte-17.d214.org>
2016-02-26 08:41:58 -0600

    Added the Outlets for each box

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var box1: UILabel!
+    @IBOutlet weak var box2: UILabel!
+    @IBOutlet weak var box3: UILabel!
+    @IBOutlet weak var box4: UILabel!
+    @IBOutlet weak var box5: UILabel!
+    @IBOutlet weak var box6: UILabel!
+    @IBOutlet weak var box7: UILabel!
+    @IBOutlet weak var box8: UILabel!
+    @IBOutlet weak var box9: UILabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
